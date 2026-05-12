3852ed70ce6ecce4f140aa2dcba16dcdbdba87bf
Ramvinay3 <ramvinay609@gmail.com>
2026-05-12 21:34:14 +0530

    setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.js"],
+
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.node
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
