b6a743b2aad4486acdfd1ee0d1e3479b5ebe91f2
Ramvinay3 <ramvinay609@gmail.com>
2026-05-12 21:37:26 +0530

    fixed jwt issue in authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const jwt= require('jsonwebtoken');
 exports.registerUser = async (req, res) => {
   try {
     const { firstName, lastName, mobile, email, password } = req.body;
