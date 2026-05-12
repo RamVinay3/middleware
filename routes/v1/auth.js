8d02f27e85563954db80167a13d753d13aa0506c
Ramvinay3 <ramvinay609@gmail.com>
2026-05-12 18:55:37 +0530

    testing ci

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -4,7 +4,7 @@ const router=express.Router();
 const authController=require('../../controllers/auth');
 const {authMiddleware} = require('../../middleware/isAuthenticated');
 
-router.post('/login',authController.loginUser);
+router.post('/login', authController.loginUser);
 router.post('/signup',authController.registerUser);
 router.post('/refreshtoken',authController.refreshAccessToken);
 router.get('/logout',authMiddleware,authController.logoutUser);
